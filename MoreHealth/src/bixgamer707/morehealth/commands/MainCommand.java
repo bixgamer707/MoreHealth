949c4857686584454add4ac6b8203666132a27b2
bixgamer 707 <85091403+bixgamer707@users.noreply.github.com>
2021-06-12 20:12:43 -0500

    Add files via upload

---
+++ MoreHealth/src/bixgamer707/morehealth/commands/MainCommand.java
@@ -0,0 +1,78 @@
+package bixgamer707.morehealth.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import bixgamer707.morehealth.ConfigFile;
+import bixgamer707.morehealth.MessagesFile;
+import bixgamer707.morehealth.MoreHealth;
+import bixgamer707.morehealth.events.InventoryHealth;
+
+public class MainCommand implements CommandExecutor{	
+private MoreHealth plugin;
+	
+	public MainCommand(MoreHealth plugin){
+		this.plugin = plugin;
+	}
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if(!(sender instanceof Player)){
+			Bukkit.getConsoleSender().sendMessage(plugin.nombre+ChatColor.RED+"You cannot run commands from the console");
+			return false;
+		}else{
+			Player player = (Player) sender;
+			ConfigFile config = new ConfigFile();
+			MessagesFile messages = new MessagesFile();
+			if(args.length > 0){
+				if(args[0].equalsIgnoreCase("version")){			
+					player.sendMessage(plugin.nombre+ChatColor.translateAlternateColorCodes('&', "&7The plugin version is &e"+plugin.version));
+					return true;				
+						}else if(args[0].equalsIgnoreCase("hearts")){
+							if(player.hasPermission("morehealth.inventoryhearts") || player.isOp()){
+								InventoryHealth inv = new InventoryHealth(plugin);
+								inv.newInventory(player);		
+								return true;
+							}else{
+								player.sendMessage(plugin.nombre+ChatColor.translateAlternateColorCodes('&', messages.getString("no-permission")));	
+								}
+						}else if(args[0].equalsIgnoreCase("editxp")) {
+							if(player.hasPermission("morehealth.admin.editxp") || player.isOp()){
+								plugin.add(player.getUniqueId());
+								player.sendMessage(plugin.nombre+ChatColor.translateAlternateColorCodes('&', "&c "+messages.getString("enter-edit-xp")));
+							}else {
+								player.sendMessage(plugin.nombre+ChatColor.translateAlternateColorCodes('&', "&c "+messages.getString("no-permission")));
+								return true;
+							}
+						}else if(args[0].equalsIgnoreCase("reload")){
+							if(player.hasPermission("morehealth.admin.reload") || player.isOp()){	
+								config.reloadFile();
+								messages.reloadFile();
+								player.sendMessage(plugin.nombre+ChatColor.translateAlternateColorCodes('&', messages.getString("reload-message")));
+							}else{
+								player.sendMessage(plugin.nombre+ChatColor.translateAlternateColorCodes('&', "&c "+messages.getString("no-permission")));
+							}				
+					}else{
+						player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&1+-------------"+plugin.nombre+"&1----------------+"));
+						player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c/mh version &7to see the plugin version"));
+						player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c/mh reload &7to load the config"));
+						player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c/mh hearts &7Open the hearts menu"));
+						player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c/hearts &7Open the hearts menu"));
+						player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&1+-------------"+plugin.nombre+"&1----------------+"));
+						return true;			
+					}
+				}else{
+					player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&1+-------------"+plugin.nombre+"&1----------------+"));
+					player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c/mh version &7to see the plugin version"));
+					player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c/mh reload &7to load the config"));
+					player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c/mh hearts &7Open the hearts menu"));
+					player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c/hearts &7Open the hearts menu"));
+					player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&1+-------------"+plugin.nombre+"&1----------------+"));
+					return true;	
+				}			
+				return true;			
+		}
+	}
+}
---
+++ MoreHealth/src/bixgamer707/morehealth/commands/Tab.java
@@ -0,0 +1,27 @@
+package bixgamer707.morehealth.commands;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.util.StringUtil;
+
+public class Tab implements TabCompleter {
+	@Override
+    public List<String> onTabComplete(CommandSender sender, Command cmd, String label, String[] args) {
+        List<String> completions = new ArrayList<>();
+        List<String> commands = new ArrayList<>();	
+	        if (args.length == 1) {
+	                commands.add("reload");
+	                commands.add("version");
+	                commands.add("hearts");
+	                commands.add("editxp");
+	            StringUtil.copyPartialMatches(args[0], commands, completions);
+	        }
+	        Collections.sort(completions);
+	        return completions;
+	}
+}
