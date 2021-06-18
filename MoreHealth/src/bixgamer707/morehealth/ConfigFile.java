c18583f0f2909efec4d220a727ce053ec6088188
bixgamer 707 <85091403+bixgamer707@users.noreply.github.com>
2021-06-17 20:15:53 -0500

    Delete MoreHealth directory

--- MoreHealth/src/bixgamer707/morehealth/ConfigFile.java
+++
@@ -1,8 +0,0 @@
-package bixgamer707.morehealth;
-
-public class ConfigFile extends Files {
-
-    public ConfigFile(){
-        super("config.yml");
-    }
-}
--- MoreHealth/src/bixgamer707/morehealth/MessagesFile.java
+++
@@ -1,8 +0,0 @@
-package bixgamer707.morehealth;
-
-public class MessagesFile extends Files {
-
-    public MessagesFile(){
-        super("messages.yml");
-    }
-}
